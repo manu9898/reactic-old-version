3e4708d078fb957d014b89ef8dcc5476656dd56f
Emmanuel Muscat <manumuscat3@gmail.com>
2023-10-13 18:47:49 +0200

    home page responsive, all that's left is to find a solution to increase the text size to iphone size. the methodology page isn't finish, we need to improve responsive on this page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
           '274px': '274px',
         },
       },
+      screens: {
+        '2xs': { min: '300px' },
+        xs: { max: '575px' }, // Mobile (iPhone 3 - iPhone XS Max).
+        sm: { min: '576px', max: '897px' }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
+        md: { min: '898px', max: '1199px' }, // Tablet (matches max: iPad Pro @ 1112px).
+        lg: { min: '1200px' }, // Desktop smallest.
+        xl: { min: '1159px' }, // Desktop wide.
+        '2xl': { min: '1359px' } // Desktop widescreen.
+      },
       translate: {
         '13': '13rem',
       },
